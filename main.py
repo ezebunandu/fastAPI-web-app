ee690e8adaa43e14cf8b4ff2bcca28953273ece4
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2021-03-13 11:57:16 -0700

    Refactor: uses chameleon to return HTML templates

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import fastapi
 import uvicorn
+import fastapi_chameleon
+import os
 
 app = fastapi.FastAPI()
+folder = os.path.dirname(__file__)
+template_folder = os.path.join(folder, "templates")
+template_folder = os.path.abspath(template_folder)
+
+fastapi_chameleon.global_init(template_folder, auto_reload=True)
 
 
 @app.get("/")
-def index():
-    content = """
-    <h1>Hello FastAPI Web App</h1>
-    <div>This is where our clone pypi app will live!</div>
-    """
-    return fastapi.responses.HTMLResponse(content=content)
+@fastapi_chameleon.template(template_file="index.html")
+def index(user: str = "anon"):
+    return {"user_name": user}
 
 
 if __name__ == "__main__":
