301f4f74e94ac5204bb0b6044515b8f913981a68
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2021-03-13 13:19:36 -0700

    Refactor: sets up separate views

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import fastapi
 import uvicorn
 import fastapi_chameleon
-import os
+
+from views import home
 
 app = fastapi.FastAPI()
-folder = os.path.dirname(__file__)
-template_folder = os.path.join(folder, "templates")
-template_folder = os.path.abspath(template_folder)
-
-fastapi_chameleon.global_init(template_folder, auto_reload=True)
+fastapi_chameleon.global_init("templates")
 
 
-@app.get("/")
-@fastapi_chameleon.template(template_file="index.html")
-def index(user: str = "anon"):
-    return {"user_name": user}
+def main():
+    configure()
+    uvicorn.run(app)
+
+
+def configure():
+    fastapi_chameleon.template(template_file="index.html")
+    app.include_router(home.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    main()
+else:
+    configure()
